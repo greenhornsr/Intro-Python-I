467be6433e0490a2a9aadce9aa58f964d53e15a6
greenhornsr <srollins11279@gmail.com>
2020-01-14 14:43:22 -0800

    scopes.py complete

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -6,25 +6,26 @@ x = 12
 
 def change_x():
     x = 99
+    print("local var x: ", x)
 
 change_x()
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
-print(x)
+print("global var x: ", x)
 
 
 # This nested function has a similar problem.
 
 def outer():
     y = 120
-
+    print("outer var y: ", y)
     def inner():
         y = 999
-
+        print("inner var y: ", y)
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
-    print(y)
+    print("Global var y: ", y)
 
 outer()
