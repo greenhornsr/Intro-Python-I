1f69b287e4a82051a0a61affafdfe2b247b9bae0
greenhornsr <srollins11279@gmail.com>
2020-01-14 14:58:35 -0800

    file.io.py complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open("foo.txt") as f:
+    read_data = f.read()
+    print(read_data)
+    f.close()
+    print("foo.txt is closed: ", f.closed)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +21,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+b = open("bar.txt", "w")
+b.write("three\nnew\nlines")
+b.close()
+print("bar.txt is closed: ", f.closed)
